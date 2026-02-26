9195fcb51257ed6d4f31dad1a4c1be1fa8ca58ed
jei <jeiffmukasa@gmail.com>
2026-02-26 11:16:11 +0300

    adding users to the system

---
+++ modules/user.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_mail import Message
+from itsdangerous import URLSafeTimedSerializer
+from config import get_connection, mail
+import datetime
+
+user_bp = Blueprint("user", __name__)
+
+# Secret serializer
+serializer = URLSafeTimedSerializer("SECRET_KEY")
